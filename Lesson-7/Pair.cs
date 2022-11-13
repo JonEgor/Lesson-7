99523b8cc991df5d6f4ae455180777f313716da8
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-11-13 11:30:56 +0300

    Добавьте файлы проекта.

---
+++ Lesson-7/Pair.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Lessons_7_;
+
+Pair<string, int> pair = new Pair<string, int>("Jmishenko",54);
+Console.WriteLine(pair.Name);
+Console.WriteLine(pair.Age);
---
+++ Lesson-7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Lessons_7_
+{
+    internal class Pair<S, T>
+    {
+        public S Name;
+        public T Age;
+        public Pair(S name, T age)
+        {
+            Name = name ;
+            Age = age;
+        }   
+    }
+
+}
+
+
